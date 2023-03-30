600f4edd215e4f9f9698a6bb17d0c84c1ee9b8df
Vaishnavi Patil <Vaishnavi Patil@SMTC-R-0205>
2023-03-30 16:28:11 +0530

    first python uploadd

---
+++ intro/selenium_automation.py
@@ -0,0 +1,9 @@
+print("hello world of selenium python")
+import time
+from selenium import webdriver
+driver = webdriver.Chrome
+driver.get("https://www.ecoders.in")
+time.sleep(5)
+print(driver.title)
+print(driver.current_url)
+driver.quit()
